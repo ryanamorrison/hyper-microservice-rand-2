b8f038a5a39a422eaf8937e30125fcbba6b453d5
ryan <ryan@samvasana.com>
2023-10-14 11:16:54 -0700

    add args configuration options

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, App};
+use dotenv::dotenv;
 use hyper::{Body,Response,Server};
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
 use log::{debug,info,trace};
+use std::env;
 
 fn main() {
+  dotenv().ok();
   //initialize logging
-  pretty_env_logger::init();
-  info!("Rando Microservice - v0.1.0");
+  env_logger::init();
+  //parse environmental vars
+  let matches = App::new(crate_name!())
+    .version(crate_version!())
+    .author(crate_authors!())
+    .about(crate_description!())
+    .arg(Arg::with_name("address")
+       .short("a")
+       .long("address")
+       .value_name("VAR_ADDRESS")
+       .help("Sets an address")
+       .takes_value(true))
+    .arg(Arg::with_name("config")
+       .short("c")
+       .long("config")
+       .value_name("VAR_CONFIG_FILE")
+       .help("Sets a custom config file")
+       .takes_value(true))
+    .get_matches(); 
+  info!("Rando Microservice - {:?}", matches.value_of("version"));
   trace!("Starting...");
-  //create address to bind to
-  let addr = ([127,0,0,1],8080).into();
+  //determine address to bind to
+  let addr = matches.value_of("address")
+    .map(|s|s.to_owned())
+    .or(env::var("VAR_ADDRESS").ok())
+    .unwrap_or_else(|| "127.0.0.1:8080".into())
+    .parse()
+    .expect("can't parse VAR_ADDRESS variable");  
   debug!("Trying to bind server to address: {}", addr);
   //create a server instance
   let builder = Server::bind(&addr);
