84706f84a067819997779b9f140348c1b6246868
ryan <ryan@samvasana.com>
2023-10-16 10:39:29 -0700

    add serde and random types

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,97 @@
-use clap::{crate_authors,crate_description,crate_name,crate_version,Arg,App};
-use dotenv::dotenv;
-use hyper::{Body,Response,Server};
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
-use log::{debug,info,trace,warn};
-use serde_derive::Deserialize;
-use std::env;
-use std::io::{self,Read};
-use std::fs::File;
-use std::net::SocketAddr;
+extern crate futures;
+extern crate hyper;
+extern crate rand;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use std::ops::Range;
+use futures::{future, Future, Stream};
+use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use hyper::service::service_fn;
+use rand::Rng;
+use rand::distributions::{Bernoulli, Normal, Uniform};
+
+#[derive(Serialize)]
+struct RngResponse {
+  value: f64,
+}
 
 #[derive(Deserialize)]
-struct Config {
-  address: SocketAddr,
+enum RngRequest {
+  Uniform {
+    range: Range<i32>,
+  },
+  Normal {
+    mean: f64,
+    std_dev: f64,
+  },
+  Bernoulli {
+    p: f64
+  },
+}
+
+static INDEX: &[u8] = b"Random Microservice";
+
+fn handle_request(request: RngRequest) -> RngResponse {
+  let mut rng = rand::thread_rng();
+  let value = {
+    match request {
+      RngRequest::Uniform { range } => {
+        rng.sample(Uniform::from(range)) as f64
+      },
+      RngRequest::Normal { mean,std_dev } => {
+        rng.sample(Normal::new(mean,std_dev)) as f64
+      },
+      RngRequest::Bernoulli { p } => {
+        rng.sample(Bernoulli::new(p)) as i8 as f64
+      },
+    }
+  };
+  RngResponse { value }
+}
+
+fn microservice_handler(req: Request<Body>) -> Box<dyn Future<Item=Response<Body>, Error=Error> + Send> {
+  match (req.method(), req.uri().path()) {
+    (&Method::GET, "/") | (&Method::GET, "/random") => {
+      Box::new(future::ok(Response::new(INDEX.into())))
+    },
+    (&Method::POST, "/random") => {
+      let body = req.into_body().concat2()
+        .map(|chunks| {
+          let res = serde_json::from_slice::<RngRequest>(chunks.as_ref())
+            .map(handle_request)
+            .and_then(|resp| serde_json::to_string(&resp));
+          match res {
+            Ok(body) => {
+              Response::new(body.into())
+            },
+            Err(err) => {
+              Response::builder()
+                .status(StatusCode::UNPROCESSABLE_ENTITY)    //HTTP 422
+                .body(err.to_string().into())
+                .unwrap()
+            },
+          }
+        });
+      Box::new(body)
+    },
+    _ => {
+      let resp = Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body("Not Found".into())
+        .unwrap();
+      Box::new(future::ok(resp))
+    },
+  }
 }
 
 fn main() {
-  dotenv().ok();
-  //initialize logging
-  env_logger::init();
-  //parse environmental vars
-  let matches = App::new(crate_name!())
-    .version(crate_version!())
-    .author(crate_authors!())
-    .about(crate_description!())
-    .arg(Arg::with_name("address")
-       .short("a")
-       .long("address")
-       .value_name("VAR_ADDRESS")
-       .help("Sets an address")
-       .takes_value(true))
-    .arg(Arg::with_name("config")
-       .short("c")
-       .long("config")
-       .value_name("VAR_CONFIG_FILE")
-       .help("Sets a custom config file")
-       .takes_value(true))
-    .get_matches(); 
-  info!("Rando Microservice - {:?}", matches.value_of("version"));
-  trace!("Starting...");
-  //open config file
-  let config = File::open("microservice.toml")
-    .and_then(|mut file| {
-      let mut buffer = String::new();
-      file.read_to_string(&mut buffer)?;
-      Ok(buffer)
-    })
-    .and_then(|buffer| {
-      toml::from_str::<Config>(&buffer)
-        .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
-    })
-    .map_err(|err| {
-      warn!("Can't read config file: {}", err);
-    })
-    .ok();
-  //determine address to bind to
-  //commandline param, environmental var, socket address, configuration file, default value
-  let addr = matches.value_of("address")
-    .map(|s|s.to_owned())
-    .or(env::var("VAR_ADDRESS").ok())
-    .and_then(|addr| addr.parse().ok())
-    .or(config.map(|config| config.address))
-    .or_else(|| Some(([127,0,0,1],8080).into()))
-    .unwrap();
-  debug!("Trying to bind server to address: {}", addr);
-  //create a server instance
+  let addr = ([127,0,0,1], 8080).into();
   let builder = Server::bind(&addr);
-  trace!("Creating service handler...");
-  //set a requests handler
   let server = builder.serve(|| {
-    service_fn_ok(|req| {
-      trace!("Incoming request is: {:?}", req);
-      let random_byte = rand::random::<u8>();
-      debug!("Generated value is: {}", random_byte);
-      Response::new(Body::from(random_byte.to_string()))
-    })
-  }); 
-  info!("Used address: {}",server.local_addr());
-  //drop any errors
+    service_fn(microservice_handler)
+  });
   let server = server.map_err(drop);
-  //add service instance to runtime
-  debug!("Run!");
   hyper::rt::run(server);
 }
